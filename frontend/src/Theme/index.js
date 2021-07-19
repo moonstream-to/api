93d353115430ec90978efc0e3ea63766f516a986
Tim Pechersky <tim.pechersky@gmail.com>
2021-07-19 20:03:31 +0800

    remove circle button for now

--- frontend/src/Theme/index.js
+++ frontend/src/Theme/index.js
@@ -1,6 +1,5 @@
 export { default as Icon } from "../components/CustomIcon";
 export { default as Button } from "./Button";
 export { default as Input } from "./Input";
-export { default as CircleButton } from "../components/CircleButton";
 export { default as JournalLinkBox } from "../components/JournalLinkBox";
 export { default as EntryCard } from "../components/EntryCard";
