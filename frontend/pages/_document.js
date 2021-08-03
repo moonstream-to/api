1e6aa0eb37897a133c1311ff0c5849dfd236d5e3
Tim Pechersky <tim.pechersky@gmail.com>
2021-08-03 23:14:07 +0800

    google analytics

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -45,7 +45,7 @@ export default class MyDocument extends Document {
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
           <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=G-ERLQ4PG62Q"
+            src="https://www.googletagmanager.com/gtag/js?id=G-MNVHX36LZ1"
           ></script>
           <script
             dangerouslySetInnerHTML={{
@@ -55,7 +55,7 @@ export default class MyDocument extends Document {
                 dataLayer.push(arguments);
               }
               gtag("js", new Date());
-              gtag("config", "G-ERLQ4PG62Q");`,
+              gtag("config", "G-MNVHX36LZ1");`,
             }}
           />
         </Head>
