04071cbe27ce9bc3194f836c85297172abf8f781
yhtiyar <yhtiyarsahatow@gmail.com>
2021-08-03 19:10:03 +0300

    removed comments

--- frontend/src/core/hooks/useTxInfo.js
+++ frontend/src/core/hooks/useTxInfo.js
@@ -8,19 +8,6 @@ const useTxInfo = (transaction) => {
     const toast = useToast();
     const getTxInfo = async () => {
         const response = await TxInfoService.getTxInfo(transaction);
-        // const responce_transaction = response.data;
-        // console.log(responce_transaction)
-        // responce_transaction.tags = [];
-        // responce_transaction.tags.append("hello", "test")
-        // if (responce_transaction.is_smart_contract_deployment) {
-        //   responce_transaction.tags.append("smart contract deployment")
-        // }
-        // if (responce_transaction.is_smart_contract_call) {
-        //   responce_transaction.tags.append("smart contract call")
-        // }
-        // if (responce_transaction.smart_contract_address) {
-        //   responce_transaction.tx.smart_contract_address = responce_transaction.smart_contract_address
-        // }
         return response.data;
     }
     const { data, isLoading, isFetchedAfterMount, refetch, isError, error } =
