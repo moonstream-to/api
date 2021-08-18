69be51d8e3cfcfd9667cf68eadfe79de4f3cc9c9
Neeraj Kashyap <nkashy1@gmail.com>
2021-08-18 12:44:12 -0700

    Added TODOs

--- crawlers/ethtxpool/main.go
+++ crawlers/ethtxpool/main.go
@@ -45,6 +45,20 @@ func main() {
 		panic(fmt.Sprintf("Invalid Humbug configuration: %s", err.Error()))
 	}
 
+	// TODO(kompotkot, zomglings): We should cache pending transactions in memory and only publish reports
+	// for new transactions. When this process starts up, it should pull the most recent (within last 5 minutes -- tunable)
+	// reported pending transactions from the Humbug journal (will require a read token) and load them
+	// into the cache.
+
+	// TODO(kompotkot, zomglings): Humbug API (on Spire) support bulk publication of reports. We should modify
+	// Humbug go client to use the bulk publish endpoint. Currently, if we have to publish all transactions
+	// pending in txpool, we *will* get rate limited. We may want to consider adding a publisher to the
+	// Humbug go client that can listen on a channel and will handle rate limiting, bulk publication etc. itself
+	// (without user having to worry about it).
+
+	// TODO(kompotkot, zomglings): Right now, we are only working with "pending" transactions. Ethereum
+	// TXPool data structure also has "queued" transactions. What is the difference between "pending" and
+	// "queued" and should we be reporting "queued" transaction as well?
 	var result map[string]map[string]map[uint64]*types.Transaction
 	gethClient.Call(&result, "txpool_content")
 	pendingTransactions := result["pending"]
