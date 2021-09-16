a093946a7a6ef6dabe86737f7132d4faf811f9c2
yhtiyar <yhtiyarsahatow@gmail.com>
2021-09-16 10:32:49 +0300

    changed summary schema, now it requires start and end blocks

--- crawlers/mooncrawl/mooncrawl/nft/cli.py
+++ crawlers/mooncrawl/mooncrawl/nft/cli.py
@@ -3,13 +3,16 @@ A command line tool to crawl information about NFTs from various sources.
 """
 import argparse
 from datetime import datetime, timedelta, timezone
+import dateutil.parser
+from dateutil.relativedelta import relativedelta
 import json
+import logging
 import os
 import sys
 import time
-from typing import Any, Dict, cast
+from typing import Any, Dict, cast, Optional
 
-import dateutil.parser
+
 from moonstreamdb.db import yield_db_session_ctx
 from moonstreamdb.models import EthereumBlock
 from sqlalchemy.orm.session import Session
@@ -20,6 +23,11 @@ from .ethereum import summary as ethereum_summary, add_labels
 from ..publish import publish_json
 from ..settings import MOONSTREAM_IPC_PATH
 from ..version import MOONCRAWL_VERSION
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+BLOCKS_PER_SUMMARY = 40
 
 
 def web3_client_from_cli_or_env(args: argparse.Namespace) -> Web3:
@@ -47,35 +55,86 @@ def get_latest_block_from_db(db_session: Session):
     )
 
 
+def get_latest_summary_block() -> Optional[int]:
+    pass
+
+
+def get_latest_nft_event_date() -> Optional[datetime]:
+    pass
+
+
+def sync_labels(
+    db_session: Session, web3_client: Web3, start: Optional[int]
+) -> EthereumBlock:
+    if start is None:
+        logger.info(
+            "Syncing start block is not given, getting it from latest nft label in db"
+        )
+        start_date = get_latest_nft_event_date()
+        if start_date is None:
+            logger.warning(
+                "Didn't find any nft labels in db, starting sync from 3 month before now"
+            )
+            time_now = datetime.now(timezone.utc)
+            start_date = time_now - relativedelta(months=3)
+
+        start = (
+            db_session.query(EthereumBlock)
+            .filter(EthereumBlock.timestamp >= start_date.timestamp())
+            .order_by(EthereumBlock.timestamp.asc())
+            .limit(1)
+            .one()
+        ).block_number
+    logger.info(f"Start block: {start}, date: {start_date}")
+    latest_block = get_latest_block_from_db(db_session)
+    end = latest_block.block_number
+    assert start <= end, f"Start block {start} is greater than latest_block {end} in db"
+    logger.info(f"Labeling blocks {start}-{end}")
+    add_labels(web3_client, db_session, start, end)
+    return latest_block
+
+
+def sync_summaries(db_session: Session, end: int, start: Optional[int]):
+    if start is None:
+        logger.info(
+            "Syncing start time is not given, getting it from latest nft label in db"
+        )
+        start = get_latest_summary_block()
+        if start is None:
+            time_now = datetime.now(timezone.utc)
+            start_date = time_now - relativedelta(months=3)
+            start = (
+                db_session.query(EthereumBlock)
+                .filter(EthereumBlock.timestamp >= start_date.timestamp())
+                .order_by(EthereumBlock.timestamp.asc())
+                .limit(1)
+                .one()
+            ).block_number
+        start += 1
+
+    while start < end:
+        current_end = start + BLOCKS_PER_SUMMARY - 1
+        current_end_time = (
+            db_session.query(EthereumBlock)
+            .filter(EthereumBlock.block_number <= current_end)
+            .order_by(EthereumBlock.block_number.desc())
+            .limit(1)
+            .one()
+        ).timestamp
+        summary_result = ethereum_summary(
+            db_session, datetime.fromtimestamp(current_end_time, timezone.utc)
+        )
+
+
 def ethereum_sync_handler(args: argparse.Namespace) -> None:
     web3_client = web3_client_from_cli_or_env(args)
+
     humbug_token = os.environ.get("MOONSTREAM_HUMBUG_TOKEN")
     if humbug_token is None:
         raise ValueError("MOONSTREAM_HUMBUG_TOKEN env variable is not set")
 
     with yield_db_session_ctx() as db_session:
-        start = args.start
-        if start is None:
-            time_now = datetime.now(timezone.utc)
-            week_ago = time_now - timedelta(weeks=1)
-            start = (
-                db_session.query(EthereumBlock)
-                .filter(EthereumBlock.timestamp >= week_ago.timestamp())
-                .order_by(EthereumBlock.timestamp.asc())
-                .limit(1)
-                .one()
-            ).block_number
-
-        latest_block = get_latest_block_from_db(db_session)
-        end = latest_block.block_number
-        assert (
-            start <= end
-        ), f"Start block {start} is greater than latest_block {end} in db"
-
         while True:
-            print(f"Labeling blocks {start}-{end}")
-            add_labels(web3_client, db_session, start, end)
-
             end_time = datetime.fromtimestamp(latest_block.timestamp, timezone.utc)
             print(f"Creating summary with endtime={end_time}")
             result = ethereum_summary(db_session, end_time)
@@ -96,36 +155,30 @@ def ethereum_label_handler(args: argparse.Namespace) -> None:
         add_labels(web3_client, db_session, args.start, args.end, args.address)
 
 
-def push_summary(result: Dict[str, Any], end_block_no: int, humbug_token: str):
+def push_summary(result: Dict[str, Any], humbug_token: str):
 
-    title = f"NFT activity on the Ethereum blockchain: end time: {result['crawled_at'] } (block {end_block_no})"
+    title = (
+        f"NFT activity on the Ethereum blockchain: end time: {result['crawled_at'] })"
+    )
     publish_json(
         "nft_ethereum",
         humbug_token,
         title,
         result,
-        tags=[f"crawler_version:{MOONCRAWL_VERSION}", f"end_block:{end_block_no}"],
+        tags=[f"crawler_version:{MOONCRAWL_VERSION}"],
         wait=False,
     )
 
 
 def ethereum_summary_handler(args: argparse.Namespace) -> None:
-    with yield_db_session_ctx() as db_session:
-        result = ethereum_summary(db_session, args.end)
 
-    # humbug_token = args.humbug
-    # if humbug_token is None:
-    #     humbug_token = os.environ.get("MOONSTREAM_HUMBUG_TOKEN")
-    # if humbug_token:
-    #     title = f"NFT activity on the Ethereum blockchain: {start_time} (block {start_block}) to {end_time} (block {end_block})"
-    #     publish_json(
-    #         "nft_ethereum",
-    #         humbug_token,
-    #         title,
-    #         result,
-    #         tags=[f"crawler_version:{MOONCRAWL_VERSION}"],
-    #         wait=False,
-    #     )
+    with yield_db_session_ctx() as db_session:
+        result = ethereum_summary(db_session, args.start, args.end)
+    humbug_token = args.humbug
+    if humbug_token is None:
+        humbug_token = os.environ.get("MOONSTREAM_HUMBUG_TOKEN")
+    if humbug_token:
+        push_summary(result, humbug_token)
     with args.outfile as ofp:
         json.dump(result, ofp)
 
@@ -181,20 +234,18 @@ def main() -> None:
         "summary", description="Generate Ethereum NFT summary"
     )
     parser_ethereum_summary.add_argument(
-        "-e",
-        "--end",
-        type=dateutil.parser.parse,
-        default=time_now.isoformat(),
-        help=f"End time for window to calculate NFT statistics (default: {time_now.isoformat()})",
+        "-s",
+        "--start",
+        type=int,
+        required=True,
+        help=f"Start block for window to calculate NFT statistics",
     )
     parser_ethereum_summary.add_argument(
-        "--humbug",
-        default=None,
-        help=(
-            "If you would like to write this data to a Moonstream journal, please provide a Humbug "
-            "token for that here. (This argument overrides any value set in the "
-            "MOONSTREAM_HUMBUG_TOKEN environment variable)"
-        ),
+        "-e",
+        "--end",
+        type=int,
+        required=True,
+        help=f"End block for window to calculate NFT statistics",
     )
     parser_ethereum_summary.add_argument(
         "-o",
@@ -202,6 +253,13 @@ def main() -> None:
         type=argparse.FileType("w"),
         default=sys.stdout,
         help="Optional file to write output to. By default, prints to stdout.",
+    )
+
+    parser_ethereum_summary.add_argument(
+        "-humbug",
+        "--humbug",
+        default=None,
+        help="Humbug token, if given summary will send there",
     )
     parser_ethereum_summary.set_defaults(func=ethereum_summary_handler)
 
