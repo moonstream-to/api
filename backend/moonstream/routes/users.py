0927a24ae77fb9cb08905794fad55a8a4243ee94
Tim Pechersky <tim.pechersky@gmail.com>
2021-09-23 15:08:07 +0200

    remove unused definition

--- backend/moonstream/routes/users.py
+++ backend/moonstream/routes/users.py
@@ -186,7 +186,6 @@ async def tokens_handler(request: Request) -> BugoutUserTokens:
 async def token_update_handler(
     request: Request, token_note: str = Form(...), access_token: str = Form(...)
 ) -> BugoutToken:
-    token = request.state.token
     try:
         response = bc.update_token(token=access_token, token_note=token_note)
     except BugoutResponseException as e:
