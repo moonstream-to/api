55c5e89bbed4334ae1245ef72b72f5d7677a0736
kompotkot <zenitsws@gmail.com>
2021-10-15 09:33:56 +0000

    Logic if block difference to large to catch up with crawler

--- crawlers/mooncrawl/mooncrawl/ethcrawler.py
+++ crawlers/mooncrawl/mooncrawl/ethcrawler.py
@@ -97,7 +97,7 @@ def ethcrawler_blocks_sync_handler(args: argparse.Namespace) -> None:
         )
         if latest_stored_block_number is None:
             latest_stored_block_number = 0
-        
+
         if latest_stored_block_number >= latest_block_number:
             logger.info(
                 f"Synchronization is unnecessary for blocks {latest_stored_block_number}-{latest_block_number - 1}"
@@ -106,18 +106,23 @@ def ethcrawler_blocks_sync_handler(args: argparse.Namespace) -> None:
             continue
 
         block_number_difference = latest_block_number - 1 - latest_stored_block_number
-
-        if args.start is None:
-            if block_number_difference < args.confirmations:
-                logger.info(
-                    f"Synchronization is unnecessary for blocks {latest_stored_block_number}-{latest_block_number - 1}"
-                )
-                time.sleep(5)
-                continue
-            else:
-                bottom_block_number = latest_stored_block_number + 1
+        if block_number_difference >= 70:
+            logger.warning(
+                f"Block difference is too large: {block_number_difference}, crawling {args.confirmations + 1} latest blocks"
+            )
+            bottom_block_number = latest_block_number - args.confirmations + 1
         else:
-            bottom_block_number = max(latest_stored_block_number + 1, args.start)
+            if args.start is None:
+                if block_number_difference < args.confirmations:
+                    logger.info(
+                        f"Synchronization is unnecessary for blocks {latest_stored_block_number}-{latest_block_number - 1}"
+                    )
+                    time.sleep(5)
+                    continue
+                else:
+                    bottom_block_number = latest_stored_block_number + 1
+            else:
+                bottom_block_number = max(latest_stored_block_number + 1, args.start)
 
         for blocks_numbers_list in yield_blocks_numbers_lists(
             f"{bottom_block_number}-{latest_block_number}",
