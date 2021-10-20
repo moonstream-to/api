4324a65ba4e505fe58a3dcd48e6cb7f8bb32433f
kompotkot <zenitsws@gmail.com>
2021-10-20 22:42:07 +0000

    Use internal ip address for geth in deploy.bash

--- nodes/deploy/deploy.bash
+++ nodes/deploy/deploy.bash
@@ -17,17 +17,23 @@ echo "Retrieving deployment parameters"
 mkdir -p "${SECRETS_DIR}"
 > "${NODE_PARAMETERS_ENV_PATH}"
 ENV_PARAMETERS=$(aws ssm describe-parameters \
-  --parameter-filters Key=tag:Product,Values=moonstream Key=tag:Blockchain,Values=ethereum \
-  | jq -r .Parameters[].Name)
+    --parameter-filters Key=tag:Product,Values=moonstream Key=tag:Blockchain,Values=ethereum \
+    | jq -r .Parameters[].Name)
 ENV_PARAMETERS_VALUES=$(aws ssm get-parameters \
-  --names $ENV_PARAMETERS \
-  --query "Parameters[*].{Name:Name,Value:Value}")
+    --names $ENV_PARAMETERS \
+    --query "Parameters[*].{Name:Name,Value:Value}")
 ENV_PARAMETERS_VALUES_LENGTH=$(($(echo $ENV_PARAMETERS_VALUES | jq length) - 1))
 for i in $(seq 0 $ENV_PARAMETERS_VALUES_LENGTH)
 do
-  param_key=$(echo $ENV_PARAMETERS_VALUES | jq -r .[$i].Name)
-  param_value=$(echo $ENV_PARAMETERS_VALUES | jq .[$i].Value)
-  echo "export $param_key=$param_value" >> "${NODE_PARAMETERS_ENV_PATH}"
+    param_key=$(echo $ENV_PARAMETERS_VALUES | jq -r .[$i].Name)
+    if [ "$param_key" == "MOONSTREAM_NODE_ETHEREUM_IPC_ADDR" ]
+    then
+        LOCAL_IP="$(curl http://169.254.169.254/latest/meta-data/local-ipv4)"
+        param_value="\"$LOCAL_IP\""
+    else
+        param_value=$(echo $ENV_PARAMETERS_VALUES | jq .[$i].Value)
+    fi
+    echo "export $param_key=$param_value" >> "${NODE_PARAMETERS_ENV_PATH}"
 done
 
 echo
