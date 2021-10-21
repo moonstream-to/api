6fad65941b784fdbfb262af6da7929635f45905b
kompotkot <zenitsws@gmail.com>
2021-10-21 10:20:39 +0000

    -e for echo

--- nodes/deploy/deploy.bash
+++ nodes/deploy/deploy.bash
@@ -52,7 +52,7 @@ done
 
 echo
 echo
-echo "${PREFIX_INFO} Replacing Ethereum Geth service definition with ${ETHEREUM_GETH_SERVICE}"
+echo -e "${PREFIX_INFO} Replacing Ethereum Geth service definition with ${ETHEREUM_GETH_SERVICE}"
 chmod 644 "${SCRIPT_DIR}/${ETHEREUM_GETH_SERVICE}"
 cp "${SCRIPT_DIR}/${ETHEREUM_GETH_SERVICE}" "/etc/systemd/system/${ETHEREUM_GETH_SERVICE}"
 systemctl daemon-reload
@@ -60,9 +60,9 @@ systemctl disable "${ETHEREUM_GETH_SERVICE}"
 
 if systemctl is-active --quiet "${ETHEREUM_GETH_SERVICE}"
 then
-    echo "${PREFIX_WARN} Ethereum Geth service ${ETHEREUM_GETH_SERVICE} already running"
+    echo -e "${PREFIX_WARN} Ethereum Geth service ${ETHEREUM_GETH_SERVICE} already running"
 else
-    echo "${PREFIX_INFO} Restart Geth service ${ETHEREUM_GETH_SERVICE}"
+    echo -e "${PREFIX_INFO} Restart Geth service ${ETHEREUM_GETH_SERVICE}"
     systemctl restart "${ETHEREUM_GETH_SERVICE}"
     sleep 10
 fi
