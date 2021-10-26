1b311a9e8f7ebf2f9df53de36a80aeb19b8cffa4
yhtiyar <yhtiyarsahatow@gmail.com>
2021-10-26 12:46:41 +0300

    added env variable to sample.env

--- backend/moonstream/web3_provider.py
+++ backend/moonstream/web3_provider.py
@@ -1,7 +1,7 @@
 from web3 import Web3
-from .settings import MOONSTREAM_WEB3_PROVIDER
+from .settings import MOONSTREAM_ETHEREUM_WEB3_PROVIDER
 
-moonstream_web3_provider = Web3(Web3.HTTPProvider(MOONSTREAM_WEB3_PROVIDER))
+moonstream_web3_provider = Web3(Web3.HTTPProvider(MOONSTREAM_ETHEREUM_WEB3_PROVIDER))
 
 
 def yield_web3_provider() -> Web3:
