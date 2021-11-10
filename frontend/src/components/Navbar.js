a45614216200ef579b391100f78dab073f43af8e
Tim Pechersky <tim.pechersky@gmail.com>
2021-11-10 16:11:06 +0000

    fix merge main mistake

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -23,30 +23,6 @@ const Navbar = () => {
       overflow="hidden"
     >
       <Suspense fallback={""}>
-        {modal === "register" && <SignUp toggleModal={toggleModal} />}
-        {modal === "login" && <SignIn toggleModal={toggleModal} />}
-        {modal === "forgot" && <ForgotPassword toggleModal={toggleModal} />}
-        {modal === "hubspot-trader" && (
-          <HubspotForm
-            toggleModal={toggleModal}
-            title={"Join the waitlist"}
-            formId={"29a17405-819b-405d-9563-f75bfb3774e0"}
-          />
-        )}
-        {modal === "hubspot-fund" && (
-          <HubspotForm
-            toggleModal={toggleModal}
-            title={"Join the waitlist"}
-            formId={"04f0b8df-6b8f-4cd0-871f-4e872523b6f5"}
-          />
-        )}
-        {modal === "hubspot-developer" && (
-          <HubspotForm
-            toggleModal={toggleModal}
-            title={"Join the waitlist"}
-            formId={"1897f4a1-3a00-475b-9bd5-5ca2725bd720"}
-          />
-        )}
         {(!isAppView || !isLoggedIn) && <LandingNavbar />}
         {isAppView && isLoggedIn && <AppNavbar />}
       </Suspense>
