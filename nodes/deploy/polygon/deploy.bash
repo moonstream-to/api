4447066a9ac5048e88e403965db354d0b1333cea
kompotkot <zenitsws@gmail.com>
2021-11-11 13:22:26 +0000

    Updated moonstream polygon node deployment script

--- nodes/deploy/polygon/deploy.bash
+++ nodes/deploy/polygon/deploy.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Deployment script - intended to run on Moonstream node control server
+# Deployment script - intended to run on Moonstream Polygon node control server
+
 # Colors
 C_RESET='\033[0m'
 C_RED='\033[1;31m'
@@ -15,10 +16,22 @@ PREFIX_CRIT="${C_RED}[CRIT]${C_RESET} [$(date +%d-%m\ %T)]"
 # Main
 AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-east-1}"
 SECRETS_DIR="${SECRETS_DIR:-/home/ubuntu/moonstream-secrets}"
-NODE_PARAMETERS_ENV_PATH="${SECRETS_DIR}/node.env"
+PARAMETERS_ENV_PATH="${SECRETS_DIR}/app.env"
 SCRIPT_DIR="$(realpath $(dirname $0))"
 BLOCKCHAIN="polygon"
 HEIMDALL_HOME="/mnt/disks/nodes/${BLOCKCHAIN}/.heimdalld"
+
+# Parameters scripts
+CHECKENV_PARAMETERS_SCRIPT="${SCRIPT_DIR}/parameters.bash"
+CHECKENV_NODES_CONNECTIONS_SCRIPT="${SCRIPT_DIR}/nodes-connections.bash"
+
+# Polygon heimdalld service files
+POLYGON_HEIMDALLD_SERVICE_FILE="heimdalld.service"
+POLYGON_HEIMDALLD_BRIDGE_SERVICE_FILE="heimdalld-bridge.service"
+POLYGON_HEIMDALLD_REST_SERVICE_FILE="heimdalld-rest-server.service"
+
+# Polygon bor service file
+POLYGON_BOR_SERVICE_FILE="bor.service"
 
 set -eu
 
@@ -26,14 +39,22 @@ echo
 echo
 echo -e "${PREFIX_INFO} Retrieving deployment parameters"
 mkdir -p "${SECRETS_DIR}"
-> "${NODE_PARAMETERS_ENV_PATH}"
+> "${PARAMETERS_ENV_PATH}"
+bash "${CHECKENV_PARAMETERS_SCRIPT}" -vn -p "moonstream" -o "${PARAMETERS_ENV_PATH}"
 
-GETH_NODE_ADDR=$(dig +short ethereum.moonstream.internal)
-GETH_NODE_PORT=$(aws ssm get-parameters --names MOONSTREAM_NODE_ETHEREUM_IPC_PORT --query "Parameters[*]" | jq -r .[0].Value)
-if [ -n "$GETH_NODE_ADDR" ] && [ -n "$GETH_NODE_PORT" ]
-then
-    MOONSTREAM_NODE_ETHEREUM_IPC_URI="http://$GETH_NODE_ADDR:$GETH_NODE_PORT"
-    echo "MOONSTREAM_NODE_ETHEREUM_IPC_URI=\"$MOONSTREAM_NODE_ETHEREUM_IPC_URI\"" >> "${NODE_PARAMETERS_ENV_PATH}"
-    sed -i "s|^eth_rpc_url =.*|eth_rpc_url = \"$MOONSTREAM_NODE_ETHEREUM_IPC_URI\"|" $HEIMDALL_HOME/config/heimdall-config.toml
-    echo -e "${PREFIX_INFO} Updated ${C_GREEN}eth_rpc_url = $MOONSTREAM_NODE_ETHEREUM_IPC_URI${C_RESET} for Heimdall"
-fi
+echo
+echo
+echo -e "${PREFIX_INFO} Updating nodes connection parameters"
+bash "${CHECKENV_NODES_CONNECTIONS_SCRIPT}" -v -f "${PARAMETERS_ENV_PATH}"
+
+echo
+echo
+echo -e "${PREFIX_INFO} Source extracted parameters"
+. "${PARAMETERS_ENV_PATH}"
+
+echo
+echo
+echo -e "${PREFIX_INFO} Update heimdall config file with Ethereum URI"
+MOONSTREAM_NODE_ETHEREUM_IPC_URI="http://$MOONSTREAM_NODE_ETHEREUM_IPC_ADDR:$MOONSTREAM_NODE_ETHEREUM_IPC_PORT"
+sed -i "s|^eth_rpc_url =.*|eth_rpc_url = \"$MOONSTREAM_NODE_ETHEREUM_IPC_URI\"|" $HEIMDALL_HOME/config/heimdall-config.toml
+echo -e "${PREFIX_INFO} Updated ${C_GREEN}eth_rpc_url = $MOONSTREAM_NODE_ETHEREUM_IPC_URI${C_RESET} for heimdall"
