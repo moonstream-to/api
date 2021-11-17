0bc8a87fee7b469352e6e1ff141ddbc586353619
Tim Pechersky <tim.pechersky@gmail.com>
2021-11-17 14:01:41 +0000

    content improvements

--- frontend/pages/welcome.js
+++ frontend/pages/welcome.js
@@ -11,8 +11,10 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  Link,
 } from "@chakra-ui/react";
 import Scrollable from "../src/components/Scrollable";
+import RouterLink from "next/link";
 
 const Welcome = () => {
   useEffect(() => {
@@ -54,7 +56,7 @@ const Welcome = () => {
                   Greetings traveler!
                 </Heading>
                 <Text fontWeight="semibold" pl={2}>
-                  We are very excited to see you onboard!
+                  We are very excited welcome you on board!
                 </Text>
 
                 <Text fontWeight="semibold" pl={2}>
@@ -121,16 +123,87 @@ const Welcome = () => {
                       </Stack>
                     </AccordionPanel>
                   </AccordionItem>
+                  <AccordionItem borderWidth={0}>
+                    <h2>
+                      <AccordionButton borderWidth={0}>
+                        <Heading as="h4" size="md">
+                          How do I build my smart contract dashboard?
+                        </Heading>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4} borderWidth={0}>
+                      <Stack direction="column">
+                        <chakra.span fontWeight="semibold" pl={2}>
+                          <Text fontWeight="bold" display="inline">
+                            1. Subscribe
+                          </Text>{" "}
+                          - First step is to tell us what addresses you want
+                          moonstream to track activity at. You can do this at{" "}
+                          <RouterLink passHref href="/subscriptions">
+                            <Link color="orange.500">subscriptions page</Link>
+                          </RouterLink>{" "}
+                          or simply by just clicking + on navigation bar of this
+                          page.
+                        </chakra.span>
+                        <chakra.span fontWeight="semibold" pl={2}>
+                          <Text fontWeight="bold" display="inline">
+                            2. Provide ABI to subscription
+                          </Text>{" "}
+                          - In order for us to know what data to track, you need
+                          to provide an ABI to subscription. You can upload one
+                          at{" "}
+                          <RouterLink passHref href="/subscriptions">
+                            <Link color="orange.500">subscriptions page</Link>
+                          </RouterLink>
+                          . If you are not sure what is abi ABI or which is
+                          correct for you - ask us on{" "}
+                          <Link
+                            color="orange.500"
+                            isExternal
+                            href={"https://discord.gg/K56VNUQGvA"}
+                          >
+                            Discord
+                          </Link>{" "}
+                          . <br />
+                          <i>
+                            N.B: Right now we have automatic dashboards only
+                            availible. This means that dashboard will contain
+                            analytics for ALL interfaces listed in ABI that was
+                            provided. I you need only fraction of it - leave
+                            these interfaces of interest in ABI when uploading
+                            and remove rest.
+                          </i>
+                        </chakra.span>
+
+                        <chakra.span fontWeight="semibold" pl={2}>
+                          <Text fontWeight="bold" display="inline">
+                            3. Create your dashboard
+                          </Text>{" "}
+                          - Press {`"New dashboard"`} on sidebar, or from + on
+                          navbar menu. Fill in name, select subscriptons you
+                          like to track to and checkbox what are you interested
+                          in. If there is no ABI - tracking contract specific
+                          elements {`won't`} be availible.
+                        </chakra.span>
+
+                        <chakra.span fontWeight="semibold" pl={2}>
+                          <Text fontWeight="bold" display="inline">
+                            4. Get some coffee -
+                          </Text>{" "}
+                          Your dashboard will appear on sidebar right away after
+                          succesfull creation. However, populating initially
+                          with data will take some time. Crawlers usually run in
+                          5 minutes cycle, they will start from newest block and
+                          will move down till genesis block. While you can make
+                          your favorite drink of choice and get back to see the
+                          dashboard!
+                        </chakra.span>
+                      </Stack>
+                    </AccordionPanel>
+                  </AccordionItem>
                 </Accordion>
               </Stack>
-              <Stack
-                px={[0, 12, null]}
-                // mt={24}
-                bgColor="gray.50"
-                borderRadius="xl"
-                boxShadow="xl"
-                py={4}
-              ></Stack>
             </Stack>
           </Fade>
         )}
