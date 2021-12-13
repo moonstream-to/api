8cbac2aab40a77a4ca95ba40ba1c3487c5c3e605
Andrey Dolgolev <andrey@simiotics.com>
2021-12-13 17:42:13 +0200

    Add fixes.

--- backend/moonstreamapi/admin/moonworm_tasks.py
+++ backend/moonstreamapi/admin/moonworm_tasks.py
@@ -77,7 +77,7 @@ def add_subscription(id: str):
         abi = json.loads(response["Body"].read())
 
         apply_moonworm_tasks(
-            subscription_type=subscription_resource.resource_data["type"],
+            subscription_type=subscription_resource.resource_data["subscription_type_id"],
             abi=abi,
             address=subscription_resource.resource_data["address"],
         )
