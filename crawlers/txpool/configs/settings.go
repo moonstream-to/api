4989e3afc1a6c98e1751970ddc93dad1b1e894d4
kompotkot <zenitsws@gmail.com>
2021-12-22 09:23:23 +0000

    Txpool now works with WEB3_URI env variable

--- crawlers/txpool/configs/settings.go
+++ crawlers/txpool/configs/settings.go
@@ -14,7 +14,6 @@ var HUMBUG_TXPOOL_TOKEN = os.Getenv("HUMBUG_TXPOOL_TOKEN")
 
 // Geth connection URL
 func GetIpcPath(blockchain string) string {
-	MOONSTREAM_NODE_IPC_ADDR := os.Getenv(fmt.Sprintf("MOONSTREAM_NODE_%s_IPC_ADDR", strings.ToUpper(blockchain)))
-	MOONSTREAM_NODE_IPC_PORT := os.Getenv(fmt.Sprintf("MOONSTREAM_NODE_%s_IPC_PORT", strings.ToUpper(blockchain)))
-	return fmt.Sprintf("http://%s:%s", MOONSTREAM_NODE_IPC_ADDR, MOONSTREAM_NODE_IPC_PORT)
+	MOONSTREAM_WEB3_PROVIDER_URI := os.Getenv(fmt.Sprintf("MOONSTREAM_%s_WEB3_PROVIDER_URI", strings.ToUpper(blockchain)))
+	return MOONSTREAM_WEB3_PROVIDER_URI
 }
