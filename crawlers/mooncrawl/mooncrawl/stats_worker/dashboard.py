b58cfd34b8616ae4a2e9ec7f9db96665cd3f762c
Andrey Dolgolev <andrey@simiotics.com>
2022-04-19 17:02:44 +0300

    Switch stats generation to use replica.

--- crawlers/mooncrawl/mooncrawl/stats_worker/dashboard.py
+++ crawlers/mooncrawl/mooncrawl/stats_worker/dashboard.py
@@ -13,7 +13,7 @@ from uuid import UUID
 
 import boto3  # type: ignore
 from bugout.data import BugoutResource, BugoutResources
-from moonstreamdb.db import yield_db_session_ctx
+from moonstreamdb.db import yield_db_read_only_session_ctx
 from sqlalchemy import Column, and_, distinct, func, text
 from sqlalchemy.orm import Query, Session
 from sqlalchemy.sql.operators import in_op
@@ -126,11 +126,7 @@ def generate_data(
     end = datetime.utcnow()
 
     time_series_subquery = db_session.query(
-        func.generate_series(
-            start,
-            end,
-            time_step,
-        ).label("timeseries_points")
+        func.generate_series(start, end, time_step,).label("timeseries_points")
     )
 
     time_series_subquery = time_series_subquery.subquery(name="time_series_subquery")
@@ -444,8 +440,7 @@ def generate_web3_metrics(
     abi_external_calls = [item for item in abi_json if item["type"] == "external_call"]
 
     extention_data = process_external(
-        abi_external_calls=abi_external_calls,
-        blockchain=blockchain_type,
+        abi_external_calls=abi_external_calls, blockchain=blockchain_type,
     )
 
     extention_data.append(
@@ -506,7 +501,7 @@ def stats_generate_handler(args: argparse.Namespace):
     """
     blockchain_type = AvailableBlockchainType(args.blockchain)
 
-    with yield_db_session_ctx() as db_session:
+    with yield_db_read_only_session_ctx() as db_session:
 
         start_time = time.time()
 
@@ -586,10 +581,7 @@ def stats_generate_handler(args: argparse.Namespace):
                         key = subscription_by_id[subscription_id].resource_data[
                             "s3_path"
                         ]
-                        abi = s3_client.get_object(
-                            Bucket=bucket,
-                            Key=key,
-                        )
+                        abi = s3_client.get_object(Bucket=bucket, Key=key,)
                         abi_json = json.loads(abi["Body"].read())
                         methods = generate_list_of_names(
                             type="function",
@@ -698,7 +690,7 @@ def stats_generate_api_task(
     Start crawler with generate.
     """
 
-    with yield_db_session_ctx() as db_session:
+    with yield_db_read_only_session_ctx() as db_session:
 
         logger.info(f"Amount of blockchain subscriptions: {len(subscription_by_id)}")
 
@@ -742,10 +734,7 @@ def stats_generate_api_task(
 
                     bucket = subscription_by_id[subscription_id].resource_data["bucket"]
                     key = subscription_by_id[subscription_id].resource_data["s3_path"]
-                    abi = s3_client.get_object(
-                        Bucket=bucket,
-                        Key=key,
-                    )
+                    abi = s3_client.get_object(Bucket=bucket, Key=key,)
                     abi_json = json.loads(abi["Body"].read())
 
                     methods = generate_list_of_names(
