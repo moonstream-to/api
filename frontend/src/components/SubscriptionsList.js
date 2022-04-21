fe9e271c2b174fea85489fdb8bc653e93dc1ba0f
Kellan Wampler <kellan@Kellans-MacBook-Pro.local>
2022-04-21 11:51:55 -0400

    Removing section 2 for mobile.

--- frontend/src/components/SubscriptionsList.js
+++ frontend/src/components/SubscriptionsList.js
@@ -83,22 +83,6 @@ const SubscriptionsList = ({ emptyCTA }) => {
               );
             })}
 
-            {/* <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex="1" textAlign="left">
-                    Section 2 title
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </AccordionPanel>
-            </AccordionItem> */}
           </Accordion>
         )}
       </>
