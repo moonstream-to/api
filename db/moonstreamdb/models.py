22faf8df1985535d9602511a8f3b834fa647c63a
Andrey Dolgolev <andrey@simiotics.com>
2022-05-05 15:33:41 +0300

    Add lint fixes.

--- db/moonstreamdb/models.py
+++ db/moonstreamdb/models.py
@@ -137,9 +137,21 @@ class EthereumLabel(Base):  # type: ignore
         nullable=False,
     )
     label = Column(VARCHAR(256), nullable=False, index=True)
-    block_number = Column(BigInteger, nullable=True, index=True,)
-    address = Column(VARCHAR(256), nullable=True, index=True,)
-    transaction_hash = Column(VARCHAR(256), nullable=True, index=True,)
+    block_number = Column(
+        BigInteger,
+        nullable=True,
+        index=True,
+    )
+    address = Column(
+        VARCHAR(256),
+        nullable=True,
+        index=True,
+    )
+    transaction_hash = Column(
+        VARCHAR(256),
+        nullable=True,
+        index=True,
+    )
     label_data = Column(JSONB, nullable=True)
     block_timestamp = Column(BigInteger, index=True)
     log_index = Column(Integer, nullable=True)
@@ -231,9 +243,21 @@ class PolygonLabel(Base):  # type: ignore
         nullable=False,
     )
     label = Column(VARCHAR(256), nullable=False, index=True)
-    block_number = Column(BigInteger, nullable=True, index=True,)
-    address = Column(VARCHAR(256), nullable=True, index=True,)
-    transaction_hash = Column(VARCHAR(256), nullable=True, index=True,)
+    block_number = Column(
+        BigInteger,
+        nullable=True,
+        index=True,
+    )
+    address = Column(
+        VARCHAR(256),
+        nullable=True,
+        index=True,
+    )
+    transaction_hash = Column(
+        VARCHAR(256),
+        nullable=True,
+        index=True,
+    )
     label_data = Column(JSONB, nullable=True)
     block_timestamp = Column(BigInteger, index=True)
     log_index = Column(Integer, nullable=True)
