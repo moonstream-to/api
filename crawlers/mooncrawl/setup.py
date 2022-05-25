a2e4565c45ba58421370eabef083eef11ebda74c
kompotkot <zenitsws@gmail.com>
2022-05-25 13:48:42 +0000

    Updated version and moonstreamdb dependence for mooncrawl package

--- crawlers/mooncrawl/setup.py
+++ crawlers/mooncrawl/setup.py
@@ -37,7 +37,7 @@ setup(
         "bugout>=0.1.19",
         "chardet",
         "fastapi",
-        "moonstreamdb>=0.2.3",
+        "moonstreamdb>=0.2.4",
         "moonworm==0.1.11",
         "humbug",
         "pydantic",
