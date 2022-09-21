8c6d22c88bc363d4347eec1579f5c3f66b292cb0
Andrey <andrey@simiotics.com>
2022-09-21 19:01:04 +0300

    Replace moonworm==0.5.1 -> moonworm[moonstream]==0.5.1. Change version.

--- crawlers/mooncrawl/setup.py
+++ crawlers/mooncrawl/setup.py
@@ -38,7 +38,7 @@ setup(
         "chardet",
         "fastapi",
         "moonstreamdb>=0.3.2",
-        "moonworm==0.5.1",
+        "moonworm[moonstream]==0.5.1",
         "humbug",
         "pydantic",
         "python-dateutil",
