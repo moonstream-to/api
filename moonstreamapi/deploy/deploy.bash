b07c77c138c4f33a71d24a23d4727bf6705da8b2
kompotkot <sumarokovsergei@yandex.ru>
2023-06-06 11:40:25 +0000

    Updated moonstreamapi placement

--- moonstreamapi/deploy/deploy.bash
+++ moonstreamapi/deploy/deploy.bash
@@ -15,7 +15,6 @@ PREFIX_CRIT="${C_RED}[CRIT]${C_RESET} [$(date +%d-%m\ %T)]"
 
 # Main
 APP_DIR="${APP_DIR:-/home/ubuntu/moonstream}"
-APP_BACKEND_DIR="${APP_DIR}/backend"
 AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-east-1}"
 PYTHON_ENV_DIR="${PYTHON_ENV_DIR:-/home/ubuntu/moonstream-env}"
 PYTHON="${PYTHON_ENV_DIR}/bin/python"
@@ -37,7 +36,7 @@ echo -e "${PREFIX_INFO} Upgrading Python pip and setuptools"
 echo
 echo
 echo -e "${PREFIX_INFO} Installing Python dependencies"
-"${PIP}" install -e "${APP_BACKEND_DIR}/"
+"${PIP}" install -e "${APP_DIR}/moonstreamapi/"
 
 echo
 echo
