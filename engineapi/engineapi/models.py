1a282c5811279349858f627e76796ac53b46bdb4
kompotkot <sumarokovsergei@yandex.ru>
2023-10-03 10:07:37 +0000

    live_at for call_requests

--- engineapi/engineapi/models.py
+++ engineapi/engineapi/models.py
@@ -315,6 +315,7 @@ class CallRequest(Base):
     parameters = Column(JSONB, nullable=False)
 
     expires_at = Column(DateTime(timezone=True), nullable=True, index=True)
+    live_at = Column(DateTime(timezone=True), server_default=utcnow(), nullable=False)
 
     created_at = Column(
         DateTime(timezone=True), server_default=utcnow(), nullable=False
