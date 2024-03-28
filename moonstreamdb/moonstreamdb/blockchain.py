d4db66c551c5892a72b1ed53f1187371627a92e9
kompotkot <sumarokovsergei@yandex.ru>
2024-03-28 12:17:20 +0000

    Models for Avalanche and zk era sepolia blockchains

--- moonstreamdb/moonstreamdb/blockchain.py
+++ moonstreamdb/moonstreamdb/blockchain.py
@@ -2,6 +2,18 @@ from enum import Enum
 from typing import Type, Union
 
 from .models import (
+    ArbitrumNovaBlock,
+    ArbitrumNovaLabel,
+    ArbitrumNovaTransaction,
+    ArbitrumSepoliaBlock,
+    ArbitrumSepoliaLabel,
+    ArbitrumSepoliaTransaction,
+    AvalancheBlock,
+    AvalancheFujiBlock,
+    AvalancheFujiLabel,
+    AvalancheFujiTransaction,
+    AvalancheLabel,
+    AvalancheTransaction,
     EthereumBlock,
     EthereumLabel,
     EthereumTransaction,
@@ -14,24 +26,21 @@ from .models import (
     WyrmBlock,
     WyrmLabel,
     WyrmTransaction,
-    XDaiBlock,
-    XDaiLabel,
-    XDaiTransaction,
-    ZkSyncEraTestnetBlock,
-    ZkSyncEraTestnetLabel,
-    ZkSyncEraTestnetTransaction,
-    ZkSyncEraBlock,
-    ZkSyncEraLabel,
-    ZkSyncEraTransaction,
-    ArbitrumNovaBlock,
-    ArbitrumNovaTransaction,
-    ArbitrumNovaLabel,
-    ArbitrumSepoliaBlock,
-    ArbitrumSepoliaTransaction,
-    ArbitrumSepoliaLabel,
     XaiBlock,
     XaiLabel,
     XaiTransaction,
+    XDaiBlock,
+    XDaiLabel,
+    XDaiTransaction,
+    ZkSyncEraBlock,
+    ZkSyncEraLabel,
+    ZkSyncEraSepoliaBlock,
+    ZkSyncEraSepoliaLabel,
+    ZkSyncEraSepoliaTransaction,
+    ZkSyncEraTestnetBlock,
+    ZkSyncEraTestnetLabel,
+    ZkSyncEraTestnetTransaction,
+    ZkSyncEraTransaction,
 )
 
 
@@ -41,11 +50,14 @@ class AvailableBlockchainType(Enum):
     MUMBAI = "mumbai"
     XDAI = "xdai"
     WYRM = "wyrm"
-    ZKSYNC_ERA_TESTNET = "zksync_era_testnet"
     ZKSYNC_ERA = "zksync_era"
+    ZKSYNC_ERA_TESTNET = "zksync_era_testnet"
+    ZKSYNC_ERA_SEPOLIA = "zksync_era_sepolia"
     ARBITRUM_NOVA = "arbitrum_nova"
     ARBITRUM_SEPOLIA = "arbitrum_sepolia"
     XAI = "xai"
+    AVALANCHE = "avalanche"
+    AVALANCHE_FUJI = "avalanche_fuji"
 
 
 def get_block_model(
@@ -59,9 +71,12 @@ def get_block_model(
         WyrmBlock,
         ZkSyncEraTestnetBlock,
         ZkSyncEraBlock,
+        ZkSyncEraSepoliaBlock,
         ArbitrumNovaBlock,
         ArbitrumSepoliaBlock,
         XaiBlock,
+        AvalancheBlock,
+        AvalancheFujiBlock,
     ]
 ]:
     """
@@ -77,9 +92,12 @@ def get_block_model(
             WyrmBlock,
             ZkSyncEraTestnetBlock,
             ZkSyncEraBlock,
+            ZkSyncEraSepoliaBlock,
             ArbitrumNovaBlock,
             ArbitrumSepoliaBlock,
             XaiBlock,
+            AvalancheBlock,
+            AvalancheFujiBlock,
         ]
     ]
     if blockchain_type == AvailableBlockchainType.ETHEREUM:
@@ -96,12 +114,18 @@ def get_block_model(
         block_model = ZkSyncEraTestnetBlock
     elif blockchain_type == AvailableBlockchainType.ZKSYNC_ERA:
         block_model = ZkSyncEraBlock
+    elif blockchain_type == AvailableBlockchainType.ZKSYNC_ERA_SEPOLIA:
+        block_model = ZkSyncEraSepoliaBlock
     elif blockchain_type == AvailableBlockchainType.ARBITRUM_NOVA:
         block_model = ArbitrumNovaBlock
     elif blockchain_type == AvailableBlockchainType.ARBITRUM_SEPOLIA:
         block_model = ArbitrumSepoliaBlock
     elif blockchain_type == AvailableBlockchainType.XAI:
         block_model = XaiBlock
+    elif blockchain_type == AvailableBlockchainType.AVALANCHE:
+        block_model = AvalancheBlock
+    elif blockchain_type == AvailableBlockchainType.AVALANCHE_FUJI:
+        block_model = AvalancheFujiBlock
     else:
         raise Exception("Unsupported blockchain type provided")
 
@@ -119,9 +143,12 @@ def get_label_model(
         WyrmLabel,
         ZkSyncEraTestnetLabel,
         ZkSyncEraLabel,
+        ZkSyncEraSepoliaLabel,
         ArbitrumNovaLabel,
         ArbitrumSepoliaLabel,
         XaiLabel,
+        AvalancheLabel,
+        AvalancheFujiLabel,
     ]
 ]:
     """
@@ -137,9 +164,12 @@ def get_label_model(
             WyrmLabel,
             ZkSyncEraTestnetLabel,
             ZkSyncEraLabel,
+            ZkSyncEraSepoliaLabel,
             ArbitrumNovaLabel,
             ArbitrumSepoliaLabel,
             XaiLabel,
+            AvalancheLabel,
+            AvalancheFujiLabel,
         ]
     ]
     if blockchain_type == AvailableBlockchainType.ETHEREUM:
@@ -156,12 +186,19 @@ def get_label_model(
         label_model = ZkSyncEraTestnetLabel
     elif blockchain_type == AvailableBlockchainType.ZKSYNC_ERA:
         label_model = ZkSyncEraLabel
+    elif blockchain_type == AvailableBlockchainType.ZKSYNC_ERA_SEPOLIA:
+        label_model = ZkSyncEraSepoliaLabel
     elif blockchain_type == AvailableBlockchainType.ARBITRUM_NOVA:
         label_model = ArbitrumNovaLabel
     elif blockchain_type == AvailableBlockchainType.ARBITRUM_SEPOLIA:
         label_model = ArbitrumSepoliaLabel
     elif blockchain_type == AvailableBlockchainType.XAI:
         label_model = XaiLabel
+    elif blockchain_type == AvailableBlockchainType.AVALANCHE:
+        label_model = AvalancheLabel
+    elif blockchain_type == AvailableBlockchainType.AVALANCHE_FUJI:
+        label_model = AvalancheFujiLabel
+
     else:
         raise Exception("Unsupported blockchain type provided")
 
@@ -179,9 +216,12 @@ def get_transaction_model(
         WyrmTransaction,
         ZkSyncEraTestnetTransaction,
         ZkSyncEraTransaction,
+        ZkSyncEraSepoliaTransaction,
         ArbitrumNovaTransaction,
         ArbitrumSepoliaTransaction,
         XaiTransaction,
+        AvalancheTransaction,
+        AvalancheFujiTransaction,
     ]
 ]:
     """
@@ -197,9 +237,12 @@ def get_transaction_model(
             WyrmTransaction,
             ZkSyncEraTestnetTransaction,
             ZkSyncEraTransaction,
+            ZkSyncEraSepoliaTransaction,
             ArbitrumNovaTransaction,
             ArbitrumSepoliaTransaction,
             XaiTransaction,
+            AvalancheTransaction,
+            AvalancheFujiTransaction,
         ]
     ]
     if blockchain_type == AvailableBlockchainType.ETHEREUM:
@@ -216,12 +259,18 @@ def get_transaction_model(
         transaction_model = ZkSyncEraTestnetTransaction
     elif blockchain_type == AvailableBlockchainType.ZKSYNC_ERA:
         transaction_model = ZkSyncEraTransaction
+    elif blockchain_type == AvailableBlockchainType.ZKSYNC_ERA_SEPOLIA:
+        transaction_model = ZkSyncEraSepoliaTransaction
     elif blockchain_type == AvailableBlockchainType.ARBITRUM_NOVA:
         transaction_model = ArbitrumNovaTransaction
     elif blockchain_type == AvailableBlockchainType.ARBITRUM_SEPOLIA:
         transaction_model = ArbitrumSepoliaTransaction
     elif blockchain_type == AvailableBlockchainType.XAI:
         transaction_model = XaiTransaction
+    elif blockchain_type == AvailableBlockchainType.AVALANCHE:
+        transaction_model = AvalancheTransaction
+    elif blockchain_type == AvailableBlockchainType.AVALANCHE_FUJI:
+        transaction_model = AvalancheFujiTransaction
     else:
         raise Exception("Unsupported blockchain type provided")
 
