a4224285bed5b5c86e820dbb47fe1c71a42e1e7c
Andrey <andrey@simiotics.com>
2024-06-14 19:15:57 +0300

    Bump version update monworm dependency.

--- crawlers/mooncrawl/setup.py
+++ crawlers/mooncrawl/setup.py
@@ -41,7 +41,7 @@ setup(
         "moonstreamdb-v3>=0.0.9",
         "moonstream-types>=0.0.3",
         "moonstream>=0.1.1",
-        "moonworm[moonstream]>=0.6.2",
+        "moonworm[moonstream]>=0.7.3",
         "humbug",
         "pydantic==1.9.2",
         "python-dateutil",
