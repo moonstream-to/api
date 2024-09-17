309f81b95cdabffc342fd044f54fb1d788023c37
Andrey <andrey@simiotics.com>
2024-09-17 15:46:23 +0300

    Update version

--- moonstreamdb-v3/moonstreamdbv3/alembic_indexes/versions/6807bdf6f417_add_deploy_block.py
+++ moonstreamdb-v3/moonstreamdbv3/alembic_indexes/versions/6807bdf6f417_add_deploy_block.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "6807bdf6f417"
-down_revision: Union[str, None] = "48d2562504d1"
+down_revision: Union[str, None] = "25b339f55f8f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
