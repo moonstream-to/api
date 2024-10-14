c2434a90d790d86e5793485aa0db3445d27b8143
kompotkot <sumarokovsergei@yandex.ru>
2024-10-14 16:00:51 +0000

    Game7 mainnet labels table

--- moonstreamdb-v3/moonstreamdbv3/models.py
+++ moonstreamdb-v3/moonstreamdbv3/models.py
@@ -1289,6 +1289,51 @@ class ImxZkevmSepoliaLabel(EvmBasedLabel):  # type: ignore
     )
 
 
+class Game7Label(EvmBasedLabel):  # type: ignore
+    __tablename__ = "game7_labels"
+
+    __table_args__ = (
+        Index(
+            "ix_game7_labels_addr_block_num",
+            "address",
+            "block_number",
+            unique=False,
+        ),
+        Index(
+            "ix_game7_labels_addr_block_ts",
+            "address",
+            "block_timestamp",
+            unique=False,
+        ),
+        Index(
+            "uk_game7_labels_tx_hash_tx_call",
+            "transaction_hash",
+            unique=True,
+            postgresql_where=text("label='seer' and label_type='tx_call'"),
+        ),
+        Index(
+            "uk_game7_labels_tx_hash_log_idx_evt",
+            "transaction_hash",
+            "log_index",
+            unique=True,
+            postgresql_where=text("label='seer' and label_type='event'"),
+        ),
+        Index(
+            "uk_game7_labels_tx_hash_tx_call_raw",
+            "transaction_hash",
+            unique=True,
+            postgresql_where=text("label='seer-raw' and label_type='tx_call'"),
+        ),
+        Index(
+            "uk_game7_labels_tx_hash_log_idx_evt_raw",
+            "transaction_hash",
+            "log_index",
+            unique=True,
+            postgresql_where=text("label='seer-raw' and label_type='event'"),
+        ),
+    )
+
+
 class Game7TestnetLabel(EvmBasedLabel):  # type: ignore
     __tablename__ = "game7_testnet_labels"
 
